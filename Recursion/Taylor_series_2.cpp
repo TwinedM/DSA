e58a2f3fa9df5691cdfd428bf4b7c0135ffcbe83
TwinedMouse <PieueshChauhan@gmail,com>
2026-01-17 18:02:27 +0530

    shit load of things

---
+++ Recursion/Taylor_series_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int f(int&x , int c){
+    c = c-1;
+    if (c==0)
+        return 1;
+    x = x+1;
+    return f(x,c)*x;
+}
+
+int main() {
+    int r;
+    int p = 5;
+    r = f(p,p);
+    cout << r;
+}
---
+++ Recursion/power_recursion.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int power(int m , int n){
+    if (n==0){
+        return 1;
+    }
+    return power(m,n-1)*m;
+}
+
+int power2(int m , int n){
+    if (n==0)
+        return 1;
+    if (n%2 == 0)
+        return power2(m*m,n/2);
+    return m*power2(m*m,(n-1)/2);
+    
+}
+
+int main()
+{
+    int r = power2(3,4);
+    printf("%d",r);
+    return 0;
+}
